c54ed99c3f646b8fea5b1ef06c7a59cfbeff248b
Shantanu Sutar <shantanusutar17@gmail.com>
2023-12-26 00:02:59 +0530

    Pagination : implementing pagination functionality with next and previous buttons

--- app/src/Components/CoinsTable/Pagination.tsx
+++ app/src/Components/CoinsTable/Pagination.tsx
@@ -29,10 +29,15 @@ const Pagination = ({ page, coins, search, setPage }: Props) => {
     pageNumArr.push(i);
   }
   return (
-    <nav aria-label="Page navigation example">
+    <nav className=" scale-110" aria-label="Page navigation example">
       <ul className="flex h-8 items-center -space-x-px text-sm">
         <li>
-          <span className=" ms-0 flex h-8 cursor-pointer items-center justify-center rounded-s-lg border border-e-0 border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+          <span
+            onClick={() => {
+              if (page > 1) setPage(page - 1);
+            }}
+            className=" ms-0 flex h-8 cursor-pointer items-center justify-center rounded-s-lg border border-e-0 border-gray-300 bg-white px-3 leading-tight text-gray-100 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
             <span className="sr-only">Previous</span>
             <svg
               className="h-2.5 w-2.5 rtl:rotate-180"
@@ -55,7 +60,12 @@ const Pagination = ({ page, coins, search, setPage }: Props) => {
         {pageNumArr.map((num) => {
           return (
             <li>
-              <span className=" flex h-8 cursor-pointer items-center justify-center border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+              <span
+                onClick={() => setPage(num)}
+                className={` flex h-8 cursor-pointer items-center justify-center border border-gray-300  px-3 leading-tight text-gray-100 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700   dark:text-gray-100 dark:hover:bg-gray-700 dark:hover:text-white  ${
+                  page === num ? "bg-gray-600" : "bg-gray-800"
+                }`}
+              >
                 {num}
               </span>
             </li>
@@ -63,7 +73,12 @@ const Pagination = ({ page, coins, search, setPage }: Props) => {
         })}
 
         <li>
-          <span className=" flex h-8 cursor-pointer items-center justify-center rounded-e-lg border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+          <span
+            onClick={() => {
+              if (page < totalPages) setPage(page + 1);
+            }}
+            className=" flex h-8 cursor-pointer items-center justify-center rounded-e-lg border border-gray-300 bg-white px-3 leading-tight text-gray-100 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
             <span className="sr-only">Next</span>
             <svg
               className="h-2.5 w-2.5 rtl:rotate-180"
